827739fc058e6cd91127c22a6e4707916e786562
nobodyzxc <dici86.chen@gmail.com>
2017-02-01 15:16:07 +0800

    fix ssl verify and IdxOutOfBorder

--- abbrev.py
+++ abbrev.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys
+import sys , ssl
 import urllib2
 from HTMLParser import HTMLParser
 
@@ -29,21 +29,30 @@ class abbParser(HTMLParser):
                 if attr == 'class' and value == 'pairDef':
                     self.getDef = True
                 if attr == 'class' and value == 'n':
-                    self.getRank = True 
+                    self.getRank = True
                 if attr == 'title' and self.getRank == True:
                     print "Rank:" , "%3s " %value.split(' ')[0] ,
                     self.getRank = False
             if tag == "a" and self.getDef == True:
                 for(attr , value) in attrs:
                     if attr == 'href':
-                        print "%-7s" %value.split('/')[1] , " " , 
-                        print "%-15s" %value.split('/')[2]
+                        if len(value.split('/')) > 1:
+                            print "%-7s" %value.split('/')[1] , " " ,
+                        if len(value.split('/')) > 2:
+                            print "%-15s" %value.split('/')[2]
                         self.getDef = False
 #######################    MAIN   ######################
 
 if __name__ == '__main__':
     req = urllib2.Request(site)
-    response = urllib2.urlopen(req)
+    #sol 1
+    context = ssl._create_unverified_context()
+    response = urllib2.urlopen(req , context = context)
+
+    #sol 2
+    #ssl._create_default_https_context = ssl._create_unverified_context
+    #response = urllib2.urlopen(req)
+
     page = response.read() #Get html file
     RSP = abbParser()
     RSP.feed(page)
